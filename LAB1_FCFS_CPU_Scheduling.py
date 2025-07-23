c293302e53bf73a34350f7aa176402f8cfcb140a
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2025-07-23 21:47:10 +0600

    Update LAB1_FCFS_CPU_Scheduling.py

--- LAB1_FCFS_CPU_Scheduling.py
+++ LAB1_FCFS_CPU_Scheduling.py
@@ -1,56 +1,50 @@
-class Process:
-    def __init__(self, pid, arrival_time, burst_time):
-        self.pid = pid
-        self.arrival_time = arrival_time
-        self.burst_time = burst_time
-        self.start_time = 0
-        self.completion_time = 0
-        self.turnaround_time = 0
-        self.waiting_time = 0
-
-def FCFS_Non_Preemptive(processes):
-    # Sort by arrival time
-    processes.sort(key=lambda p: p.arrival_time)
-
-    clock = 0
-
-    total_tat = 0  # Total Turnaround Time
-    total_wt = 0   # Total Waiting Time
-
-    for p in processes:
-        # If the CPU is idle
-        if clock < p.arrival_time:
-            clock = p.arrival_time
-
-        p.start_time = clock
-        p.completion_time = clock + p.burst_time
-        p.turnaround_time = p.completion_time - p.arrival_time
-        p.waiting_time = p.turnaround_time - p.burst_time
-
-        clock = p.completion_time
-
-        total_tat += p.turnaround_time
-        total_wt += p.waiting_time
-
-    # Print the results
-    print("PID\tAT\tBT\tCT\tTAT\tWT")
-    for p in processes:
-        print(f"{p.pid}\t{p.arrival_time}\t{p.burst_time}\t{p.completion_time}\t{p.turnaround_time}\t{p.waiting_time}")
-
-    avg_tat = total_tat / len(processes)
-    avg_wt = total_wt / len(processes)
-
-    print(f"\nAverage Turnaround Time: {avg_tat:.2f}")
-    print(f"Average Waiting Time: {avg_wt:.2f}")
-
-# Taking user input
-n = int(input("Enter number of processes: "))
-processes = []
+n = int(input("Enter number of processes : "))
+d = dict()
 
 for i in range(n):
-    pid = f"P{i}"
-    arrival_time = int(input(f"Enter arrival time for process {pid}: "))
-    burst_time = int(input(f"Enter burst time for process {pid}: "))
-    processes.append(Process(pid, arrival_time, burst_time))
+    key = "P" + str(i + 1)
+    a = int(input("Enter arrival time of process " + str(i + 1) + ": "))
+    b = int(input("Enter burst time of process " + str(i + 1) + ": "))
+    d[key] = [a, b]
 
-FCFS_Non_Preemptive(processes)
+PS_sort = sorted(d.items(), key=lambda item: item[1][0])
+
+CT = []
+for i in range(len(PS_sort)):
+    if i == 0:
+        CT.append(PS_sort[i][1][1])
+    else:
+        CT.append(max(CT[i - 1], PS_sort[i][1][0]) + PS_sort[i][1][1])
+
+TAT = []
+for i in range(len(PS_sort)):
+    TAT.append(CT[i] - PS_sort[i][1][0])
+
+WT = []
+for i in range(len(PS_sort)):
+    WT.append(TAT[i] - PS_sort[i][1][1])
+
+avg_WT = sum(WT) / n
+
+
+print("\n-----------------------------------------------------------------------")
+print("| Process   | Arrival AT | BurstT | CompTime | TurnATime | WaitingTime|")
+print("\n-----------------------------------------------------------------------")
+
+for i in range(n):
+    print("| {:<9} | {:<10} | {:<6} | {:<8} | {:<9} | {:<10} |".format(
+        PS_sort[i][0],
+        PS_sort[i][1][0],
+        PS_sort[i][1][1],
+        CT[i],
+        TAT[i],
+        WT[i]
+    ))
+
+print("\n-----------------------------------------------------------------------")
+print("\nAverage Waiting Time: {:.2f}".format(avg_WT))
+
+print("By -")
+print("Name : MD. Raafiullah Al Naim")
+print("ID : 22101150")
+
